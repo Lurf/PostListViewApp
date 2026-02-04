222b8ff7718f3aa9345e2b2a65933aa34bafeed7
Lurf <lurf@icloud.com>
2026-02-04 22:04:12 +0900

    Add PostListViewApp

---
+++ PostListView/App/PostListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostListViewApp.swift
+//  PostListView
+//
+//  Created by Lurf on 2026/02/03.
+//
+
+import SwiftUI
+
+@main
+struct PostListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PostListView()
+        }
+    }
+}
